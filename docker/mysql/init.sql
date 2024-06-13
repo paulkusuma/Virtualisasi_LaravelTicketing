0a003e1ff5ddb4c9805995d24a1573652e83601e
paulkusuma <paul.kusuma10@gmail.com>
2024-06-13 22:10:02 +0700

    Memperbaiki serta menambahkan beberapa komentar

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,4 +1,11 @@
+-- Membuat database baru bernama 'laravel' jika belum ada
 CREATE DATABASE IF NOT EXISTS laravel;
+
+-- Membuat pengguna baru 'paulkusuma' dengan kata sandi 'pauwerfull'
 CREATE USER 'paulkusuma'@'%' IDENTIFIED BY 'pauwerfull';
+
+-- Memberikan semua hak istimewa pada semua tabel di database 'laravel' kepada pengguna 'paulkusuma'
 GRANT ALL PRIVILEGES ON laravel.* TO 'paulkusuma'@'%';
+
+-- Memperbarui hak istimewa sehingga perubahan berlaku segera
 FLUSH PRIVILEGES;
